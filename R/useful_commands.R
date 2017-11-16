71e460f4362bf7ce4749a6f08022c257fdbbef8e
oursu <oanamursu@gmail.com>
2017-11-16 12:49:25 -0800

    more code

--- R/useful_commands.R
+++ R/useful_commands.R
@@ -1,4 +1,36 @@
+require(cba)
 
 #split column by a delimiter
-x=c('test,test2','test3,test4')
-data.frame(do.call('rbind', strsplit(as.character(x),',',fixed=TRUE)))
+split_keep_one_part=function(v,delimiter,keep){
+   desired_v=data.frame(do.call('rbind', strsplit(as.character(v),delimiter,fixed=TRUE)))
+   return(desired_v[,keep])
+}
+
+keep_best=function(dataset,column,decreasing,use_abs_value,dup_col){
+   data[,column]=as.numeric(as.character(data[,column]))
+   if (use_abs_value==TRUE) {
+      data[,column]=abs(as.numeric(as.character(data[,column])))
+   }
+   sorted=data[order(data[,column]),]
+   if (decreasing==TRUE){
+      sorted=data[order(data[,column],decreasing=TRUE),]
+   }
+   dup=which(duplicated(sorted[,dup_col]))
+   if (length(dup)>0){
+      sorted=sorted[-dup,]
+   }
+   return(sorted)
+}
+
+optimal_ordering=function(m,meth){
+  if (dim(m)[1]<=2){
+  return (m)
+  }
+  require(cba)
+  d=dist(as.matrix(m),method=meth)
+  hc=hclust(d)
+  co=order.optimal(d, hc$merge)
+  m.optimalRows=as.matrix(m)[co$order,]
+  return(m.optimalRows)
+}
+					  
---
+++ generic_python.py
@@ -0,0 +1,10 @@
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--sum')
+                
+    args = parser.parse_args()
+
+main()
